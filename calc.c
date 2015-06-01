f7b3aca2ec026e1f7101211ee888d0eb82958467
vivekannan <vivek7641@gmail.com>
2015-06-01 20:57:08 +0530

    Shunt Yard algo working.

--- calc.c
+++ calc.c
@@ -1,41 +1,145 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-static const char* HELP_TEXT = "This is help.";
+static char* HELP_TEXT = "This is help.";
 static int USE_DOUBLE = 0;
 static int USE_NEWLINE = 0;
 
-void printHelp() {
+void printMessage(char* message, int exitCode) {
 	
-	printf("%s\n", HELP_TEXT);
-	exit(0);
+	printf("%s\n", message);
+	exit(exitCode);
 }
 
-char* parseOptions(int argc, char* argv[]) {
+int parseOptions(int argc, char* argv[]) {
 	
-	int i;
-	
-	for(i = 1; i < argc; i++) {
+	for(int i = 1; i < argc; i++) {
 		if(strcmp(argv[i], "-d") == 0)
 			USE_DOUBLE = 1;
 		
 		else if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
-			printHelp();
+			printMessage(HELP_TEXT, 1);
 		
 		else if(strcmp(argv[i], "-n") == 0)
 			USE_NEWLINE = 1;
 		
 		else
-			return argv[i];
+			return i;
 	}
 	
-	return (char*) 0;
+	printMessage("calc: Expression missing.\nUsage: calc [OPTIONS] EXPRESSION.\nTry 'calc --help' for more information.", -1);
+}
+
+char* appendArgs(int c, int argc, char* argv[]) {
+	
+	char* dest;
+	int length = 1;
+	
+	for(int i = c; i < argc; i++)
+		length += strlen(argv[i]);
+	
+	dest = (char*) malloc(length * sizeof(char));
+	
+	for(int i = c; i < argc; i++)
+		strcat(dest, argv[i]);
+	
+	return dest;
+}
+
+int precedence(char c) {
+	
+	switch(c) {
+		case '^':
+			return 2;
+		case '*':
+		case '/':
+		case '%':
+			return 1;
+		case '+':
+		case '-':
+			return 0;
+	}
+}
+
+int isOperator(char c) {
+	
+	return (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^');
+}
+
+int leftAssociative(char c) {
+	
+	switch(c) {
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '%':
+			return 1;
+		case '^':
+			return 0;
+	}
+}
+
+char* shuntYard(char* expr) {
+	
+	char c;
+	int sp = 0, op = 0;
+	char* stack = (char*) malloc(strlen(expr) * sizeof(char));
+	char* output = (char*) malloc(strlen(expr) * sizeof(char));
+	
+	while((c = *expr++) != '\0') {
+		
+		
+		if(isspace(c))
+			continue;
+		
+		else if(isdigit(c))
+			output[op++] = c;
+		
+		else if(c == '(')
+			stack[sp++] = c;
+		
+		else if(c == ')') {
+			while(stack[--sp] != '(') {
+				output[op++] = stack[sp];
+				
+				if(sp == 0)
+					return "Mismatched ')'!";
+			}
+			stack[sp] = '\0';
+		}
+		
+		else if(isOperator(c)) {
+			if(leftAssociative(c))
+				while(isOperator(stack[--sp]) && precedence(c) <= precedence(stack[sp]))
+					output[op++] = stack[sp];
+			
+			else
+				while(isOperator(stack[--sp]) && precedence(c) < precedence(stack[sp]))
+					output[op++] = stack[sp];
+			
+			stack[++sp] = c;
+			stack[++sp] = '\0';
+		}
+	}
+	
+	while(--sp > -1) {
+		if(stack[sp] == '(')
+			return "Mismatched '('";
+		
+		output[op++] = stack[sp];
+	}
+	
+	return output;
 }
 
 int main(int argc, char* argv[]) {
 	
-	parseOptions(argc, argv);
+	char* expr = appendArgs(parseOptions(argc, argv), argc, argv);
+	
+	printf("%s\n", shuntYard(expr));
 	
 	return 0;
 }
